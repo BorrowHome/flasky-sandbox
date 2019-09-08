bca0cb8f37f381f3af9e3d2ad15dd7fc8ee55c5f
liliangbin <2286824491@qq.com>
2019-09-08 23:47:46 +0800

    系统崩溃，我还是使用Windows吧，找个时间新建一个系统

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'init a python virtual enviroment '
+
+python -m venv venv 
+
+pip install -r requirments.txt
+
+echo 'create and done '
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export FLASK_APP=flasky.py
+export FLASK_DEBUG=1
+
+echo 'run a flask app '
+
+flask run
