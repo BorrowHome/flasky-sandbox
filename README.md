abab3cf342af06cdbe88f9cce3a55fcd019ac053
liliangbin <2286824491@qq.com>
2019-09-09 00:17:35 +0800

    主页出来了，  第一版成功

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flasky-sandbox
 沙箱沙子轮廓检测客户断
+
+
+- conda env export > environment.yaml
+- conda env create -f environment.yaml
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from app.speech import Speech
 from . import main
 from .. import db
 from ..models import User
 from flask import render_template, request
 import time
 from ..models import PoseToLocation
-from ..move_base_goal import RosGoal
 
 
 @main.route('/')
@@ -23,23 +21,3 @@ def query():
     db.session.commit()
     print 'commit '
     return render_template('web_wideo.html')
-
-
-@main.route('/asr')
-def asr():
-    audio = request.args.get('audio')
-    print audio
-    if audio is None:
-        return render_template('asr.html')
-    else:
-        speech = Speech()
-        result = speech.asr(audio)
-        print result  # we will know the message in the pcm file
-        pose_to_location = PoseToLocation.query.filter_by(location=result).first()
-        print pose_to_location.linear_x + 'LOCATION '
-
-        # INFO // liliangbin 将位置信息发送给项目  但是我们没有做一个验证，就是怎么来确定这个用户确实是拿到了数据
-        ros_goal = RosGoal()
-        ros_goal.publish_goal(pose_to_location)
-
-        return render_template('asr.html')
