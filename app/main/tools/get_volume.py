588f3741d9f2a728dea613d9a2dd5fce5b1e97b5
liliangbin <2286824491@qq.com>
2020-10-11 09:59:07 +0800

    一些没有添加的东西

--- app/main/tools/get_volume.py
+++ app/main/tools/get_volume.py
@@ -1,12 +1,12 @@
 import cv2
 from flask import request
 
+from app.main import main
 from app.utils.areas_height import areaRect
 from app.utils.areas_height.areaS import ostu
 from app.utils.frame.site import Site
 from app.utils.frame.sub import PictureSub
 from config import Config
-from app.main import main
 
 
 @main.route("/get_volume/", methods=['POST', 'GET'])
@@ -14,9 +14,9 @@ def get_volume():
     image_path = Config.UPLOAD_IMAGE_PATH
     document_path = Config.SAVE_DOCUMENT_PATH
     id = request.form['id']
-    s = cv2.imread(image_path + "iblack_" + id + ".png")
+    s = cv2.imread(image_path + "ipaint_" + id + ".png") #ipaint 是直接减去的图像
     sub = PictureSub()
-    t = sub.iblack(s, 220)
+    t = sub.iblack(s, 220) # 图像变为黑白两种
     # cv2.imwrite(image_path + "iblack_" + id + ".png", t)
 
     with open(document_path + "site_" + id + ".txt", "r+") as f:
@@ -28,9 +28,9 @@ def get_volume():
                                          frame_location.locate_x + frame_location.move_x,
                                          frame_location.locate_y + frame_location.move_y)
 
-
-    img=t
+    img = t
     sand_area = ostu(img)
+    print(sand_area)
     sand_frame_scale = float(sand_area) / float(frame_area)
     print(sand_frame_scale)
     print("sand_frame_scale")
