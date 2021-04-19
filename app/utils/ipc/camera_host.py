bc8e157f4b9ab1ea197bb9cdeb91908962f168dc
233313 <233313@nd>
2021-04-19 13:48:39 +0800

    '添加畸变矫正算法’

--- app/utils/ipc/camera_host.py
+++ app/utils/ipc/camera_host.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from config import Config
 
@@ -14,18 +15,35 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
 
+    def undistort(self, img):
+        fx = 1000
+        cx = 960
+        fy = 1000
+        cy = 540
+        k1, k2, p1, p2, k3 = -0.195, 0.05, 0, 0, 0
+
+        # 相机坐标系到像素坐标系的转换矩阵
+        k = np.array([
+            [fx, 0, cx],
+            [0, fy, cy],
+            [0, 0, 1]
+        ])
+        # 畸变系数
+        d = np.array([
+            k1, k2, p1, p2, k3
+        ])
+        h, w = img.shape[:2]
+        mapx, mapy = cv2.initUndistortRectifyMap(k, d, None, k, (w, h), 5)
+        return cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+
     def get_frame(self):
         success, image = self.video.read()
+        cv2.imwrite('C:\abc.jpg', image)
+        image = self.undistort(image)
         # 因为opencv读取的图片并非jpeg格式，因此要用motion JPEG模式需要先将图片转码成jpg格式图片
-        if success:
-            # image = cv2.flip(image, 180)  #  转换方向 180 度
-            # 存储了对应的文件
-            ret, jpeg = cv2.imencode('.jpg', image)
-            # cv2.imwrite(filename=self.file_location + self.name.strip() + '.png', img=image)
-            return jpeg.tobytes()
-        else:
-            print('can not get ')
-            return None
+        # image = cv2.flip(image, 180)
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
 
     def show(self):
         while 1:
@@ -33,8 +51,6 @@ class VideoCamera(object):
             cv2.imshow("cap", frame)
             if cv2.waitKey(100) & 0xff == ord('q'):
                 break
-
-
 
 
 if __name__ == '__main__':
